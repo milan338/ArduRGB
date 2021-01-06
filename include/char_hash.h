698fa9ce3f2c076476bb0c116bd6419e9bb6aff4
milan338 <56327606+milan338@users.noreply.github.com>
2021-01-06 19:21:32 +1030

    refactor

--- include/char_hash.h
+++ include/char_hash.h
@@ -3,10 +3,10 @@
 
 #include <Arduino.h>
 
-constexpr inline uint32_t hash(const char *effect, uint32_t i = 0)
+constexpr inline uint32_t hash(const char *str, uint32_t i = 0)
 {
     // Create numerical hash of character array
-    return !effect[i] ? 5381 : (hash(effect, i + 1) * 33) ^ effect[i];
+    return !str[i] ? 5381 : (hash(str, i + 1) * 33) ^ str[i];
 }
 
 #endif //HASH_H
