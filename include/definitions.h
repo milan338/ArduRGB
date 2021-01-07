8110f7c7f66e8bca3c275d8af1f63517fae8e878
milan338 <56327606+milan338@users.noreply.github.com>
2021-01-07 15:54:29 +1030

    Updated license

--- include/definitions.h
+++ include/definitions.h
@@ -1,9 +1,25 @@
-// Global definitions
+// This file is part of ArduRGB, an app to control lighting devices.
+// Copyright (C) 2021 Milan Sorak.
+//
+// ArduRGB is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ArduRGB is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ArduRGB.  If not, see <https://www.gnu.org/licenses/>.
 
+// ------------ Global Definitions ------------ //
 #define MESSAGE_TYPE _SERIAL   // Communication interface
 #define FADE_BY 4            // Controls fade speed between colours
 #define EFFECT_NAME_CHARS 16 // Number of chars allowed for effect mode - 1
 #define ARGS_NUM 8           // Number of additional effect arguments allowed - 1
+// ------------ Global Definitions ------------ //
 
 // ------------------ SERIAL ------------------ //
 #define SERIAL_BEGIN 254     // Message start byte
