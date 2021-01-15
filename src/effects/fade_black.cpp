97d7810afb63f0f6c13d2659181d5d578f5f4606
milan338 <56327606+milan338@users.noreply.github.com>
2021-01-15 17:21:35 +1030

    Implemented new led functions to work with reversed strips

--- src/effects/fade_black.cpp
+++ src/effects/fade_black.cpp
@@ -16,6 +16,7 @@
 
 #include <Arduino.h>
 #include <FastLED.h>
+#include "led_util.h"
 #include "fade_black.h"
 
 void FadeBlack::run(CRGBSet &led_array, uint32_t led_num, uint32_t &current_mode)
@@ -25,10 +26,10 @@ void FadeBlack::run(CRGBSet &led_array, uint32_t led_num, uint32_t &current_mode
     // Fade LED strip to black
     for (uint8_t i = 0; i < 256 / FADE_BY; i++)
     {
-        fadeToBlackBy(led_array, led_num, FADE_BY);
+        _fadeToBlackBy(led_array, led_num, FADE_BY);
         FastLED.show();
     }
     // Ensure all LEDs cleared
-    fill_solid(led_array, led_num, CRGB(0, 0, 0));
+    _fillSolid(led_array, led_num, CRGB(0, 0, 0));
     FastLED.show();
 }
--- src/effects/solid_color.cpp
+++ src/effects/solid_color.cpp
@@ -15,7 +15,7 @@
 // along with ArduRGB.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <FastLED.h>
-#include <definitions.h>
+#include "led_util.h"
 #include "solid_color.h"
 
 void SolidColor::run(CRGBSet &led_array, uint32_t led_num, uint32_t &current_mode, uint8_t led_args[])
@@ -23,6 +23,6 @@ void SolidColor::run(CRGBSet &led_array, uint32_t led_num, uint32_t &current_mod
     // Set current mode to none
     current_mode = 0;
     // Set strip colors
-    fill_solid(led_array, led_num, CRGB(led_args[0], led_args[1], led_args[2]));
+    _fillSolid(led_array, led_num, CRGB(led_args[0], led_args[1], led_args[2]));
     FastLED.show();
 }
