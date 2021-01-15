3a99ea3f3d6b046bf7bcee9336a40b132512bf0c
milan338 <56327606+milan338@users.noreply.github.com>
2021-01-15 14:47:26 +1030

    Implemented device identification output

---
+++ src/serial/serial_write.h
@@ -0,0 +1,24 @@
+// This file is part of ArduRGB, an app to control lighting devices.
+// Copyright (C) 2021 Milan Sorak.
+//
+// ArduRGB is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ArduRGB is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ArduRGB.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef SERIALWRITE_H
+#define SERIALWRITE_H
+
+#include <Arduino.h>
+
+void sendSerial(const __FlashStringHelper *message);
+
+#endif // SERIALWRITE_H
