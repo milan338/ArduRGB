a445a18072026920899a54ed36712a80f0b69ccd
milan338 <56327606+milan338@users.noreply.github.com>
2021-01-24 13:59:12 +1030

    Comment formatting cleanup

--- include/definitions.h
+++ include/definitions.h
@@ -14,14 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with ArduRGB.  If not, see <https://www.gnu.org/licenses/>.
 
-// ------------ Global Definitions ------------ //
+// Global Definitions
 #define FADE_BY 4            // Controls fade speed between colours
 #define EFFECT_NAME_CHARS 16 // Number of chars allowed for effect mode - 1
 #define ARGS_NUM 8           // Number of additional effect arguments allowed - 1
-// ------------ Global Definitions ------------ //
 
-// ------------------ SERIAL ------------------ //
+// SERIAL
 #define SERIAL_BEGIN 254     // Message start byte
 #define SERIAL_TERMINATE 255 // Message end byte
 #define SERIAL_TIMEOUT 1000  // Serial timeout in ms
-// -------------------------------------------- //
